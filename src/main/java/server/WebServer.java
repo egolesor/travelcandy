e045efde17f69ee5c6078e502ec192844fb8bfa5
Ehsan Golesorkhi <e.golesor@gmail.com>
2016-09-02 01:47:01 +0200

    spring is configured

---
+++ src/main/java/server/WebServer.java
@@ -0,0 +1,24 @@
+package server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("{server}")
+public class WebServer extends SpringBootServletInitializer{
+
+    private static Class<WebServer> applicationClass = WebServer.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+}
---
+++ src/main/java/server/home/Home.java
@@ -0,0 +1,4 @@
+package server.home;
+
+public class Home {
+}
