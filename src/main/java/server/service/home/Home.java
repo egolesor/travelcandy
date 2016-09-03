8be53a66126cb29b414147327569f3969f8e956f
Ehsan Golesorkhi <e.golesor@gmail.com>
2016-09-03 14:31:20 +0200

    to lear how interceptors are working and a little basic of thymeleaf

--- src/main/java/server/service/home/Home.java
+++ src/main/java/server/service/home/Home.java
@@ -1,15 +1,22 @@
 package server.service.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Controller
 @RequestMapping(path = "/home")
+@SessionScope
 public class Home {
 
     @RequestMapping(method = RequestMethod.GET, path = "/welcome")
-    public String greeting(){
+    public String greeting(Model model){
+
+        model.addAttribute("name", "Ehsan");
+
+        System.out.println("FUXK");
         return "home/welcome";
     }
 
